2af9cf5c7b85d3d9135d240f3f87c99887a2c03a
Ricardo Soto <ricardosotovasquez97@gmail.com>
2023-08-13 20:45:56 -0400

    Health bar update
    - Add health bar on the screen.
    - fix asteroid interaction with bullets
    - fix bullets interaction in "Game Over Screen"

--- Assets/Scripts/BulletSpawnerScript.cs
+++ Assets/Scripts/BulletSpawnerScript.cs
@@ -10,27 +10,33 @@ public class BulletSpawnerScript : MonoBehaviour
     public float spawnTime;
     private float timer = 0;
     public float bulletForce;
+    public LogicScript logic;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0)){
-            if(timer < spawnRate)
+        if(logic.healthAmount > 0f)
+        {
+            if (Input.GetMouseButton(0))
             {
-                timer = timer + Time.deltaTime;
-            }
-            else
-            {
-                GameObject bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, 2.0f), transform.rotation);
-                Rigidbody2D myRigibody = bullet.GetComponent<Rigidbody2D>();
-                myRigibody.AddForce(transform.up * bulletForce, ForceMode2D.Impulse);
-                timer = 0;
+                if (timer < spawnRate)
+                {
+                    timer = timer + Time.deltaTime;
+                }
+                else
+                {
+                    GameObject bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, 2.0f), transform.rotation);
+                    Rigidbody2D myRigibody = bullet.GetComponent<Rigidbody2D>();
+                    myRigibody.AddForce(transform.up * bulletForce, ForceMode2D.Impulse);
+                    timer = 0;
+                }
             }
         }
+        
     }
 }
